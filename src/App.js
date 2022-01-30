daad911fc5381eb97040eec52a58da4fc5732b7c
shlok <shlokjjw@gmail.com>
2022-01-30 20:31:32 +0530

    added logindoc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import LOGIN_PATIENT from './pages/js/LOGIN_PATIENT';
 import Error from './pages/js/Error';
 import About from './About';
-import Login_doctor from './pages/js/Login_doctor';
+import Logindoctor from './pages/js/Login_doctor';
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/about' element={<About/>} />
         <Route path='/login' element={<LOGIN_PATIENT/>} />
-        <Route path='/logindoc' element={<Login_doctor/>} />
+        <Route path='/logindoc' element={<Logindoctor/>} />
         <Route path='*' element={<Error/>}></Route>
       </Routes>
   );
